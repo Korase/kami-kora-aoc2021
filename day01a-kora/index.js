429f99efad1f4755cf1bccf2fd04ddd6b3ccb05a
Shiiyu <100887466+Korase@users.noreply.github.com>
2022-05-16 02:59:17 -0400

    Remove duplicate files
    Co-authored-by: VlxtIykg <VlxtIykg@users.noreply.github.com>

--- day01a-kora/index.js
+++
@@ -1,9 +0,0 @@
-import * as fs from 'fs';
-import {windows} from '../utility.mjs';
-
-fs.readFile('./input.txt', 'utf8', (err, data) => {
-  console.log(
-    "Num of Larger Measurements: "
-    + windows(data.split("\n").map(n => Number(n)), 2).filter(d => d[0] < d[1]).length
-  );
-});
